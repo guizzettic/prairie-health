2a58617182eccec9b78168900c525bbd2614ced7
Chris Guizzetti <guizzettic@gmail.com>
2022-03-22 15:01:08 -0700

    added data file, and initial App styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
+import data from './data.json';
+import { makeStyles } from '@material-ui/core/styles';
+import Chart from './Components/Chart';
+console.log(data);
+
+const useStyles = makeStyles({
+  container: {
+    height: '100vh',
+    width: '100vw',
+  },
+  header: {
+    height: 100,
+    backgroundColor: 'green',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    fontSize: '24px',
+    color: 'white',
+    fontWeight: 700,
+    letterSpacing: 2,
+  },
+});
 
 function App() {
-  return <div className="App"></div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.header}>Mental Health Progress</div>
+      {/* HELLO */}
+      {/* <div style={{ width: 900, height: 600 }}> */}
+      <Chart data={data} />
+      {/* </div> */}
+    </div>
+  );
 }
 
 export default App;
