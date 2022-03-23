eb9694c52cd19667f540d0ff86b040a73be5859a
Chris Guizzetti <guizzettic@gmail.com>
2022-03-23 08:53:15 -0700

    updated header to match requirements

---
+++ src/Components/Header.js
@@ -0,0 +1,38 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  header: {
+    height: 100,
+    backgroundColor: 'green',
+    color: 'white',
+    fontWeight: 700,
+    letterSpacing: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    paddingTop: 10,
+    textAlign: 'center',
+    fontSize: '24px',
+  },
+  subTitle: {
+    textAlign: 'center',
+    fontSize: '18px',
+    marginTop: -5,
+  },
+});
+
+const Header = ({}) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.header}>
+      <h1 className={classes.title}>Prairie Health Progress</h1>
+      <h2 className={classes.subTitle}>
+        Keep track of how your Progress is trending
+      </h2>
+    </div>
+  );
+};
+export default Header;
