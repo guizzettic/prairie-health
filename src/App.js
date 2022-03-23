d482d1b0aa3e4171694910488f71b032e20a6f95
Chris Guizzetti <guizzettic@gmail.com>
2022-03-22 17:04:34 -0700

    using json data and formatting it in App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,78 @@
 import './App.css';
-import data from './data.json';
+import cusData from './data.json';
 import { makeStyles } from '@material-ui/core/styles';
 import Chart from './Components/Chart';
-console.log(data);
+import Header from './Components/Header';
+import { useState, useEffect } from 'react';
 
 const useStyles = makeStyles({
   container: {
     height: '100vh',
     width: '100vw',
   },
-  header: {
-    height: 100,
-    backgroundColor: 'green',
-    display: 'flex',
-    justifyContent: 'center',
-    alignContent: 'center',
-    fontSize: '24px',
-    color: 'white',
-    fontWeight: 700,
-    letterSpacing: 2,
-  },
 });
 
 function App() {
   const classes = useStyles();
+  const [users, setUsers] = useState(null);
+  const [userListInfo, setUserListInfo] = useState({});
+  const [chosenUser, setChosenUser] = useState(0);
+
+  function groupUserData(data) {
+    let userList = {};
+
+    for (let key of data) {
+      let currName = key['Patient Name'],
+        gad = ['GAD-7 Score'],
+        phq = ['PHQ-9 Score'],
+        date = key.Timestamp.split(' ')[0];
+
+      if (!userList[currName]) {
+        userList[currName] = {
+          [gad]: {
+            id: gad[0],
+            color: 'hsl(231, 70%, 50%)',
+            data: [{ x: date, y: key[gad] }],
+          },
+          [phq]: {
+            id: phq[0],
+            color: 'hsl(231, 70%, 50%)',
+            data: [key[phq]],
+          },
+        };
+      } else {
+        userList[currName][gad].data.push({ x: date, y: key[gad] });
+        userList[currName][phq].data.push({ x: date, y: key[phq] });
+      }
+    }
+
+    setUsers(Object.keys(userList));
+    setUserListInfo(userList);
+    return userList;
+  }
+  useEffect(() => {
+    groupUserData(cusData);
+  }, []);
+
+  const formatData = (obj) => {
+    let userArr = [];
+
+    userArr.push(obj['GAD-7 Score']);
+    userArr.push(obj['PHQ-9 Score']);
+
+    return userArr;
+  };
+
+  if (users) formatData(userListInfo[users[chosenUser]]);
 
   return (
     <div className={classes.container}>
-      <div className={classes.header}>Mental Health Progress</div>
-      {/* HELLO */}
-      {/* <div style={{ width: 900, height: 600 }}> */}
-      <Chart data={data} />
-      {/* </div> */}
+      <Header />
+      {users && <h2>Current User: {users[chosenUser]}</h2>}
+
+      <div style={{ width: 900, height: 600 }}>
+        <Chart newData={formatData(userListInfo[users[2]])} />
+      </div>
     </div>
   );
 }
